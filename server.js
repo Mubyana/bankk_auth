1c025943423ea6c4e5f9aaccf06364712b3c2951
Mubyana <muby3720@yahoo.com>
2021-09-12 14:53:09 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,17 +3,25 @@ var bodyParser = require('body-parser');
 var MongoClient = require('mongodb').MongoClient;
 //const users = require('./mongoose/user');
 const uuidv4 = require('uuid/v4');
+const cors = require('cors');
 
 
 var app = express();
-app.use(bodyParser.json());
 
 
 app.all('*', function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "*");
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(cors({credentials: true, origin: true}))
+
+app.use(express.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 
 const mongoose = require('mongoose');
